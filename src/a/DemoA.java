2d39c4b5fd36fa12c45457c0effed4dceec48385
Brian <Brian@10.0.0.67>
2024-01-03 19:30:59 -0700

    Implemented add()

--- src/a/DemoA.java
+++ src/a/DemoA.java
@@ -5,6 +5,12 @@ public class DemoA {
 	public static void main(String[] args) {
 		
 		System.out.println("Sprint 1...");
+		System.out.println("Sprint 2...");
+	}
+	
+	//Sprint 2 automation
+	public static void add(int a, int b) {
+		System.out.println(a+b);
 	}
 
 }
